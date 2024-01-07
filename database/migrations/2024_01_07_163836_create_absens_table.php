f28208db488ca17d13b93ddf30b17a84c3f558f4
Fikri Nurhakim <108182945+akufikri@users.noreply.github.com>
2024-01-08 00:51:22 +0700

    🚀 New [ Can absens proccessed ]

---
+++ database/migrations/2024_01_07_163836_create_absens_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('absens', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('meeting_id');
+            $table->boolean('states')->default(false);
+            $table->string('optional')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('absens');
+    }
+};
