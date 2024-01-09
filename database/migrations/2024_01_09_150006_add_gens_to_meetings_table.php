4b32c8da636987069ebe2d6de9ed0910f8099b93
Fikri Nurhakim <108182945+akufikri@users.noreply.github.com>
2024-01-09 22:00:44 +0700

    🛠 Fixing [ Fixing field gen in table meeting ]

--- database/migrations/2024_01_09_150006_add_gens_to_meetings_table.php
+++ database/migrations/2024_01_09_150006_add_gens_to_meetings_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('meetings', function (Blueprint $table) {
-            $table->string("gen");
+            $table->string("gens");
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('meetings', function (Blueprint $table) {
-            $table->string("gen");
+            $table->string("gens");
         });
     }
 };
